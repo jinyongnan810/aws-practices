aae03612a672393869ef75eee000a8ced5385388
Kin Yuunan <yuunan.kin@gmail.com>
2026-07-13 19:43:39 +0900

    update: IAM part2

--- IAM/new-user/main.tf
+++ IAM/new-user/main.tf
@@ -14,3 +14,60 @@ resource "aws_iam_user_policy_attachment" "reader_attach" {
   user       = aws_iam_user.read_only_user.name
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
+
+// MARK: New Policy for S3 Uploads
+
+# Define the Policy Document (The "What")
+data "aws_iam_policy_document" "s3_upload_doc" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::company-upload-bucket/*"
+    ]
+  }
+}
+
+# Create the IAM Policy Resource
+resource "aws_iam_policy" "s3_upload_policy" {
+  name        = "S3UploadOnlyPolicy"
+  description = "Allows writing objects to the company-upload-bucket"
+  policy      = data.aws_iam_policy_document.s3_upload_doc.json
+}
+
+# Attach the New Policy to the Existing User (The "Who")
+resource "aws_iam_user_policy_attachment" "app_reader_s3_attach" {
+  user       = aws_iam_user.read_only_user.name
+  policy_arn = aws_iam_policy.s3_upload_policy.arn
+}
+
+// MARK: trust relationship and attach policy to role
+
+# Define the Trust Relationship (The "Who is allowed to put this on?")
+data "aws_iam_policy_document" "ec2_trust_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"] # The specific API call to assume a role
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"] # We are trusting AWS EC2
+    }
+  }
+}
+
+# Create the IAM Role
+resource "aws_iam_role" "app_server_role" {
+  name               = "AppServerRole"
+  assume_role_policy = data.aws_iam_policy_document.ec2_trust_policy.json
+}
+
+# Attach existing custom policy to the new Role
+resource "aws_iam_role_policy_attachment" "role_s3_attach" {
+  role = aws_iam_role.app_server_role.name
+
+  # We can reuse the policy we created in the previous step!
+  policy_arn = aws_iam_policy.s3_upload_policy.arn
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,16 @@
 # Terraform AWS Practices
 
+## Use common plugin
+Following this to avoid duplicated terraform-provider-aws_v6.54.0_x5 in each folder.
+```bash
+# common cache folder
+mkdir -p ~/.terraform.d/plugin-cache
+
+vi ~/.terraformrc
+# Add the following to ~/.terraformrc
+plugin_cache_dir = "$HOME/.terraform.d/plugin-cache"
+```
+
 ## Commands often used
 - `terraform init` - Initialize the Terraform working directory.
 - `terraform plan` - Generate and show an execution plan.
